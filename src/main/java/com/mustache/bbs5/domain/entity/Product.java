258d5ccbb11cd03a55251fe6edba20836b0bf7b9
soonmineom <skdlfma123@gmail.com>
2022-11-14 11:19:51 +0900

    product entity 만들기

---
+++ src/main/java/com/mustache/bbs5/domain/entity/Product.java
@@ -0,0 +1,27 @@
+package com.mustache.bbs5.domain.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="product") // 생략 가능
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private  String name;
+
+    @Column(nullable = false)
+    private  Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+}
